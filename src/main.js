3911d359dfade431482b17fccdb86b7dda473f94
Mohammad Ali <mdali2016.227@gmail.com>
2022-01-09 13:02:26 +0600

    [Add] Bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue'
+
 
 Vue.config.productionTip = false
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/css/style.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
 
 new Vue({
   render: h => h(App),
